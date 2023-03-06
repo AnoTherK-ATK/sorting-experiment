76aee5d7a2c20cb26e1839201b6c54d3ba6cdc52
AnoTherK-ATK <khanhvu212@gmail.com>
2023-03-06 21:47:34 +0700

    first commit/initialize

---
+++ code/checker.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    freopen("test1.txt","r",stdin);
+    for(int i = 0; i < 1000000; ++i){
+        double x;
+        cin >> x;
+        cout << x << " ";
+    }
+}
---
+++ code/generator.cpp
@@ -0,0 +1,32 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+int main(int argc, char *argv[]){
+    registerGen(argc, argv, 1);
+
+    //Increasing number
+    std::ofstream out1("1-increasing.txt");
+    for(int i = 0; i < 1000000; ++i){
+        double value = i + rnd.next(0.0000, 0.9999);
+        out1 << value << ' ';
+    }
+    out1.close();
+
+    //Decreasing number
+    std::ofstream out2("2-decreasing.txt");
+    for(int i = 1000000; i > 0; --i){
+        double value = i + rnd.next(0.0000, 0.9999);
+        out2 << value << ' ';
+    }
+    out2.close();
+
+    //Random number
+    for(int tc = 3; tc <= 10; ++tc){
+        std::string file_name = std::to_string(tc) + "-random.txt";
+        std::ofstream out(file_name);
+        for(int i = 0; i < 1000000; ++i){
+            double value = rnd.next(0.0000, 1000000.000);
+            out << value << ' ';
+        }
+        out.close();
+    }
+}
